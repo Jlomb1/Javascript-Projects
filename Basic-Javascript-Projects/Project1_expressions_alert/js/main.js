732f2af8ef010af0688d134af5d9b591fa7b7d1c
Jlomb1 <127466981+Jlomb1@users.noreply.github.com>
2023-04-18 23:37:00 -0600

    Basic Javascript Projects Assignment
    Attached are the main.js file and HTML

---
+++ Basic-Javascript-Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,23 @@
+//Assigning STRINGS to VARIABLES -------------------------------------------------------------------
+var A = "Hello! Here is an example of an \"alert box\".";
+var B = "Here is an example of assigning string to a variable,";
+var C = " Pretty neat!";
+var D = " A wise man once said, \"If not us, then who; if not now, then when?\""
+var Sent1 = "This is the beginning of the string,"
+var Sent2 = " and this is the end of the string."
+
+//Utilizing a function ------------------------------------------------------------------------------
+function My_First_Function () { //Here we are defining a function and naming it
+    var str = "This is the button text!"; //Here we are defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML = str; //We are putting the value of the variable INTO the HTML elementFromPoint with the "Button_Text" id
+}
+
+//Code that calls the assigned variables/string -----------
+window.alert(A);
+document.write(B);
+document.write(C);
+document.write(D);
+//An example of concatenating two variabes "Sent1" and "Sent2"
+document.write(Sent1+Sent2);
+//An example of an expression 9+10 which will display as 19------------------------------------------
+document.write(9+10);
